d28c45aef88cfb2f7e5c9c20e7345e7a40410bba
saihnii4 <81787757+dulguuncodes@users.noreply.github.com>
2023-01-29 12:20:42 +0800

    completed main script

--- scripts/main.py
+++ scripts/main.py
@@ -1,29 +1,3 @@
-# import camelot
-# import re
-# import json
-
-# regex = re.compile("[\d-]")
-
-# school = camelot.read_pdf("sch.pdf")
-
-# subjects = []
-
-# subjects = [*zip(school[0].df[2], school[0].df[1], school[0].df[0])][4:]
-
-# # cleanse subjects
-# subjects = [*map(lambda subj: [*filter(lambda x: x,
-#                  map(lambda _: regex.sub("", _).strip(), subj))], subjects)]
-
-# schedule = school[0].df.loc[:, 6:18]
-# periods = []
-
-# for period in range(6, 18):
-#     nth_period = map(lambda x: None if not x else x, schedule[period][3:].tolist())
-#     periods.append([*zip(nth_period, subjects)])
-
-# with open("data.json", "w") as buf:
-#     json.dump(periods, buf)
-
 import camelot
 
 import functools
@@ -31,12 +5,7 @@ import json
 import re
 
 regex = re.compile("[\d]")
-_rege = re.compile(r"(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY)")
-
-try:
-    pdf = camelot.read_pdf("sch.pdf", pages='1,2,3,4,5')
-except UserWarning:
-    pass
+pdf = camelot.read_pdf("schedule.pdf", pages='1,2,3,4,5')
 
 append_lambda = (lambda O, x: (lambda _, O: O)(O.append(x), O))
 extend_lambda = (lambda O, x: (lambda _, O: O)(O.extend(x), O))
@@ -44,49 +13,51 @@ extend_lambda = (lambda O, x: (lambda _, O: O)(O.extend(x), O))
 data = []
 
 for i in range(pdf.n):
-    pdf[i].to_json('_dump%d.json' % i)
-    with open('_dump%d.json' % i, 'r') as _buf:
+    pdf[i].to_json('./tmp/_dump%d.json' % i)
+    with open('./tmp/_dump%d.json' % i, 'r') as _buf:
         data.extend([json.load(_buf)])
 
-_acc = 0
-
-# data = functools.reduce(lambda arr, page: extend_lambda(
-#     arr, [*map(lambda kv: [*kv.values()], page)]), data, [])
-
-days = []
-
-for page in data:
-    page = [*map(lambda kv: [*kv.values()], page)]
-
-    (day_markers) = functools.reduce(lambda acc, x: append_lambda(
-        acc, x[0]+_acc) if x[1] == '1' else acc, enumerate(page[1]), [])
-
-    _acc += len(page[1])
-
-    days = extend_lambda(days, day_markers)
-
-intervals = [*zip(days, days[1:]), (days[-1],)]
-
-monday_schedule = []
+# fetch teacher roster
+subjects = []
 init_page = [*map(lambda kv: [*kv.values()], data[0])]
-for someting in init_page[3:]:
+for i, someting in enumerate(init_page[3:]):
     teacher, *subject = [*filter(lambda x: x, regex.sub("",
                                  " ".join(someting[:3]), 999).split(" "))]
-    monday_schedule.append(
-        (teacher, " ".join(subject), someting[intervals[0][0]:intervals[0][1]]))
 
-print(monday_schedule)
+    subjects.append((teacher, " ".join(subject), i))
 
-# time_slots = slices[0]
 
-# periods = [[], []]
-# for i, slot in enumerate(time_slots):
-#     if slot == '1':
-#         periods[0].append(i)
+schedule_compendium = [[]]
 
-# print(periods)
+for page in data:
+    page = [*map(lambda kv: [*kv.values()], page)][1:]
+    for teacher, subject, i in subjects:
+        if len(schedule_compendium)-1 < i:
+            schedule_compendium.append([])
+        schedule_compendium[i].extend(page[i])
 
-# pdf[0].to_json('dump.json')
+(day_markers) = functools.reduce(lambda acc, x: append_lambda(
+    acc, x[0]) if x[1] == '1' else acc, enumerate(schedule_compendium[0]), [])
 
-# with open('dump.json', 'r') as buf:
-#     data = json.load(buf)
+# we no longer need periods here (note that [1:] + [2:] = [3:])
+schedule_compendium = schedule_compendium[2:]
+intervals = [*zip(day_markers, day_markers[1:]),
+             (day_markers[-1], len(schedule_compendium[3]))]
+
+week_schedule = {}
+
+terms = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]
+
+for interval, term in zip(intervals, terms):
+    start, end = interval
+
+    week_schedule[term] = []
+
+    for teacher, subject, i in subjects:
+        if len(schedule_compendium)-1 < i:
+            continue
+        week_schedule[term].append({"subject": subject,
+                                    "teacher": teacher, "schedule": schedule_compendium[i][start:end]})
+
+with open("./tmp/processed.json", "w") as final:
+    json.dump(week_schedule, final)
